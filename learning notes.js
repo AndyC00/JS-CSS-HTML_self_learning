6e1f141dd917d7130121c5942c95860d53409a0b
AndyC00 <chenjinzhi96@gmail.com>
2024-11-27 13:52:54 +1300

    lesson 1

--- learning notes.js
+++ learning notes.js
@@ -1,3 +1,8 @@
-document.write("Hello world");
+// Primitive Value (Parameters store in Stack):
+// Types: Number, Boolean, String, undefined, null
+//      It's unable to change, represent the value itself, store in the stack
 
-var a = 1;
+// Reference Value (Parameters in Heap):
+// Types: Array, Object, Function
+//      The value stores a reference pointing to the object store in the memory, not store the object itself
+
