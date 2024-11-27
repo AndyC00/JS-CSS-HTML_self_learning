33bc40dddc697d1323b177549919050ba1edc02e
AndyC00 <chenjinzhi96@gmail.com>
2024-11-27 18:08:08 +1300

    lesson 2

--- learning_notes.js
+++ learning_notes.js
@@ -6,3 +6,9 @@
 // Types: Array, Object, Function
 //      The value stores a reference pointing to the object store in the memory, not store the object itself
 
+
+// var a = 1;
+// var b = --a + --a;
+// document.write(b);   //will return -1
+
+var score = parseInt(window.prompt('input'));
